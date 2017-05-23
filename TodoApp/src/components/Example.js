ac8d8a67e63dd812476b493e47c58ab44158d672
fearclear <fearcleari@gmail.com>
2017-05-23 16:39:44 +0800

    init

---
+++ TodoApp/src/components/Example.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+    <div>
+      Example
+      测试内容
+    </div>
+  );
+};
+
+Example.propTypes = {
+};
+
+export default Example;
